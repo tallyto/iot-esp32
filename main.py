e37fbbc4b37de80f011d96ba5d9cebccf4c4f9f5
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2024-04-09 20:43:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,56 @@
 import machine
 import time
+import dht
+import urequests
+from internet_connection import connection
 
+d = dht.DHT11(machine.Pin(4))
 r = machine.Pin(2, machine.Pin.OUT)
 
+def sendTemperatureAndHumidityToThingSpeak(api_key, temperature, humidity):
+    url = 'https://api.thingspeak.com/update?api_key={}&field1={}&field2={}'.format(api_key, temperature, humidity)
+    response = urequests.get(url)
+    print(response.text)
+    response.close()
+
+station = connection("KAMILA", '12345678')
+if not station.isconnected():
+    print("Erro de conexão com a rede WIFI! Tente novamente.")
+else:
+    print("Conexão realizada com sucesso!")
+#     station.disconnect()
+
+
 while True:
-    r.value(1)
-    time.sleep(2)
-    r.value(0)
-    time.sleep(2)
+    d.measure()
+    t = d.temperature()
+    h = d.humidity()
+    
+    sendTemperatureAndHumidityToThingSpeak('your_api_key', t,h)
+      
+    print("Disciplina: Cloud, IoT e Indústria 4.0 - 2024/1º")
+    print("Professor: Valmir Moro")
+    print("-" * 50)
+    print("Equipe")
+    print("Aluno: Tállyto Sousa Rodrigues")
+    print("-" * 50)
+    print("Climate Season")
+    print("Temperatura Atual: {}°C".format(t))
+    print("Umidade Relativa do Ar Atual: {}%".format(h))
+    print("-" * 50)
+    print("Condições de Acionamento do Relé")
+    print("Temperatura > 31°C.")
+    print("Umidade Relativa do Ar > 70%.")
+    print()
+   
+    if(t >= 31 or h >= 70):
+        r.value(1)
+        print("Rele ligado")
+    else:
+        r.value(0)
+        print("Rele desligado")
+
+    
+    time.sleep(5)
+
+
